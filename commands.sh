f207f08a2b5d4d7ebb762612dcb26c3abedbe088
Reuben Cummings <reubano@gmail.com>
2016-12-18 01:37:01 +0300

    Update data and fetch script

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-curl https://api.github.com/users/nerevu/repos?per_page=100 -o data/nerevu-repos.json
-curl https://api.github.com/users/reubano/repos?per_page=100 -o data/reubano-repos.json
+curl https://api.github.com/users/nerevu/repos?per_page=100 | jq . > data/nerevu-repos.json
+curl https://api.github.com/users/reubano/repos?per_page=100 | jq . > data/reubano-repos.json
 
 curl -d "method=flickr.photosets.getList" \
      -d "api_key=76ca8dd185de46fdd0d24c24f6e4d0ea" \
@@ -19,6 +19,15 @@ curl -d "method=flickr.photosets.getPhotos" \
      https://api.flickr.com/services/rest \
      | jq . > data/travel-gallery.json
 
+curl -d "method=flickr.photosets.getPhotos" \
+     -d "api_key=76ca8dd185de46fdd0d24c24f6e4d0ea" \
+     -d "user_id=47144176@N00" \
+     -d "extras=description,date_upload,date_taken,original_format,o_dims,media,url_o" \
+     -d "format=json" \
+     -d "nojsoncallback=1" \
+     -d "photoset_id=72157676561966910" \
+     https://api.flickr.com/services/rest \
+     | jq . > data/post-images-gallery.json
 
 curl -d "method=flickr.photosets.getPhotos" \
      -d "api_key=76ca8dd185de46fdd0d24c24f6e4d0ea" \
